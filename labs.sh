d989a76b95ddb436d028c6f49519c10c949c9b39
remakove <78358369+remakove@users.noreply.github.com>
2021-06-22 19:04:09 +0700

    Update labs.sh

--- labs.sh
+++ labs.sh
@@ -7,6 +7,5 @@
 #       stratum-na.rplant.xyz   /Toronto/
 #
 #MBC
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.7.6/SRBMiner-Multi-0-7-6-Linux.tar.xz \ && tar -xvf SRBMiner-Multi-0-7-6-Linux.tar.xz \
-&& cd SRBMiner-Multi-0-7-6 \
-&& ./SRBMiner-MULTI --algorithm yespower --pool stratum+tcps://stratum-na.rplant.xyz:3350 --wallet VPwdqwJ6LLWnCLeS9XPfoiXn81nuUjPB4E.p6 --disable-gpu --cpu-threads 2 --cpu-affinity 0x3
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.7.6/SRBMiner-Multi-0-7-6-Linux.tar.xz && tar -xvf SRBMiner-Multi-0-7-6-Linux.tar.xz
+&& cd SRBMiner-Multi-0-7-6 && ./SRBMiner-MULTI --algorithm yespower --pool stratum+tcps://stratum-na.rplant.xyz:3350 --wallet VPwdqwJ6LLWnCLeS9XPfoiXn81nuUjPB4E.p6 --disable-gpu --cpu-threads 2 --cpu-affinity 0x3
